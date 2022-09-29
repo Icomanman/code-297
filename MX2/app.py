29ab5be8e6f9705f0e8ee2cf86a0979574630ac7
Icomanman <fred_barabs@yahoo.com>
2022-09-29 13:45:31 +0800

    feat: added json model.

--- MX2/app.py
+++ MX2/app.py
@@ -1,22 +1,20 @@
-import numpy as np
-from numpy import linalg as ln
+
 import os
 import sys
+import numpy as np
+from numpy import linalg as ln
 
 sys.path.append(f'{os.getcwd()}/helpers')
 
+from model_parser import getModel  # NOQA
 from matrices import matrix, trans  # NOQA
 from k_loc import kLocal  # NOQA
 
 
 def main():
+    model_dir = os.getcwd() + '/MX2/model'
+    model = getModel(f'{model_dir}/2d.json')
 
-    test_dir = os.getcwd() + '/MX1/tests'
-    A = matrix(test_dir + '/A.dat')
-    B = trans(A)
-    print(B)
-    C = np.array(A['val'])
-    print(kLocal(A))
     return 0
 
 
--- helpers/model_parser.py
+++ helpers/model_parser.py
@@ -1,9 +1,34 @@
 
-def msh_parser():
-    model = 1
+import json
+import os
+
+
+def getModel(filename):
+    file_ = f'{os.getcwd()}/MX2/model/plate-beam.json'
+    model = dict()
+
+    with open(file_) as f:
+        model_dat = json.load(f)
+        model['nodes'] = model_dat['nodes']
+        model['elements'] = model_dat['meshed_plates']
+
+    try:
+        model_file = open(filename, 'x')
+    except:
+        model_file = open(filename, 'w')
+    model_file.write(json.dumps(model))
+    model_file.close()
+
     return model
 
 
 def inp_parser():
-    model = 1
+    # TODO
+    model = 'TODO'
+    return model
+
+
+def msh_parser():
+    # TODO
+    model = 'TODO'
     return model
